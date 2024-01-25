10af9b724f518f05e9b51c0f23265fe1df542da7
Pratyush Raj <pratyush2331@gmail.com>
2024-01-25 12:05:52 +0530

    improved movement

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -15,27 +17,27 @@ public class PlayerMovement : MonoBehaviour
     {
         if (Input.GetKeyDown("space"))
         {
-            GetComponent<Rigidbody>().velocity = new Vector3(0, 5, 0);
+            rb.velocity = new Vector3(rb.velocity.x, 5, rb.velocity.z);
         }
 
         if (Input.GetKey("up") || Input.GetKey("w"))
         {
-            GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 5);
+            rb.velocity = new Vector3(0, 0, 5);
         }
 
         if (Input.GetKey("right") || Input.GetKey("d"))
         {
-            GetComponent<Rigidbody>().velocity = new Vector3(5, 0, 0);
+            rb.velocity = new Vector3(5, 0, 0);
         }
 
         if (Input.GetKey("left") || Input.GetKey("a"))
         {
-            GetComponent<Rigidbody>().velocity = new Vector3(-5, 0, 0);
+            rb.velocity = new Vector3(-5, 0, 0);
         }
 
         if (Input.GetKey("down") || Input.GetKey("s"))
         {
-            GetComponent<Rigidbody>().velocity = new Vector3(0, 0, -5);
+            rb.velocity = new Vector3(0, 0, -5);
         }
     }
 }
